0dd9b258c6fbea48704aab921134bac7aa2035b3
Riya Israni <89697896+israni21@users.noreply.github.com>
2023-05-06 19:29:12 +0530

    Update HeroImage3.js

--- src/components/HeroImage3.js
+++ src/components/HeroImage3.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import "./HeroImageStyles3.css";
+import Img1 from "../assets/img1.png";
+import Img2 from "../assets/img2.png";
+import Img3 from "../assets/img3.png";
+import Img4 from "../assets/img4.png";
 
 const HeroImage3 = () => {
   return (
@@ -7,16 +11,17 @@ const HeroImage3 = () => {
       <div className= "footer-container2">
             <div className = "projects">
                 <h1>Projects</h1>
-                <p2></p2>
-                <p></p>
-                <p1>It is an app which allows the user to login into their Github
-                    account and displays all the information of the user's git history.</p1>
+                <p>Dalle Clone</p>
+                <p1>It is a website which allows the users to generate high-quality images from textual descriptions, 
+                    similar to the original DALL-E model. </p1>
                 <h3>Description:</h3>
-                <p2>When a user logins a dashboard appears which can display information about the User Info, 
-                    Repositories and Issue of a Project. It has a very user-friendly interface which allows the user 
-                    to easily navigate through the application in a very efficient manner.</p2>
+                <p2>This website provides an implementation of the DALL-E model architecture developed by OpenAI.
+                This website offer a user-friendly interface that allows users to input their textual descriptions and view 
+                the corresponding generated images.</p2>
                 <h3>Tech Stack:</h3>
-                <p2>Github Api, JSON Parsing, Retrofit Library, Figma for UI.</p2>
+                <p2>HTML, TailWind CSS, JavaScript, ReactJs, ExpressJs, OpenAI &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p2>
+                <a href = "https://github.com/israni21/DALL-E-CLONE"><img id = "img1" src = {Img1} alt = "Img1"></img></a>
+                
 
                 <h2>My Portfolio</h2>
                 <p1>It is a well-designed portfolio website showcasing my skills, work and achievements to 
@@ -26,7 +31,20 @@ const HeroImage3 = () => {
                     such as education and internship experience. The goal of this responsive portfolio website is to showcase
                     my expertise, creativity and unique selling points.</p2>
                 <h3>Tech Stack:</h3>
-                <p2>HTML, CSS, JavaScript, ReactJs, ExpressJs</p2>
+                <p2>HTML, CSS, JavaScript, ReactJs, ExpressJs &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p2>
+                <a href = "https://github.com/israni21/My-Portfolio"><img id = "img2" src = {Img2} alt = "Img2"></img></a>
+               
+                <h2>&#8826;Codinf&#8827;</h2>
+                <p1>It is an app which allows the user to login into their Github
+                    account and displays all the information of the user's git history.</p1>
+                <h3>Description:</h3>
+                <p2>When a user logins a dashboard appears which can display information about the User Info, 
+                    Repositories and Issue of a Project. It has a very user-friendly interface which allows the user 
+                    to easily navigate through the application in a very efficient manner.</p2>
+                <h3>Tech Stack:</h3>
+                <p2>Github Api, JSON Parsing, Retrofit Library, Figma for UI.</p2>
+                <a href = "https://github.com/israni21/Codinf"><img id = "img3" src = {Img3} alt = "Img3"></img></a>
+
                 <h2>Planning Zilla</h2>
                 <p1>It is an app which allows the user to find the
                     information regarding their career according to their interests
@@ -38,16 +56,9 @@ const HeroImage3 = () => {
                 <h3>Tech Stack:</h3>
                 <p2>This software uses the waterfall model for meeting the
                     desired requirements.</p2>
+                    <a href = "https://github.com/israni21"><img id = "img4" src = {Img4} alt = "Img4"></img></a>
 
-                <h2>NovelGram</h2>
-                <p1>It is a book reading application which enables the users to browse through a variety of categories and genres.
-                Users can easily navigate through the categories and find books that interest them.</p1>
-                <h3>Description:</h3>    
-                <p2>The app provides an extensive collection of books spanning across various categories, 
-                    including fiction, thriller/mystery, horror, romance and many more. In order to make user 
-                    journey easy and quick, few filters have been added which allows visitors to narrow in on the specific book according to their interest.</p2>
-                <h3>Teck Stack:</h3>
-                <p2>Java, Android, Figma for UI.</p2>
+                
             </div>
       </div>
     </div>
